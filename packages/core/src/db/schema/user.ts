6ddfe4fae5b4a32a4bb9ad1aacf82942cee99294
mateusz.jarzebski <matjarzebski@gmail.com>
2026-01-05 21:13:06 +0100

    make user email requiered

--- packages/core/src/db/schema/user.ts
+++ packages/core/src/db/schema/user.ts
@@ -1,12 +1,17 @@
 import * as z from "zod";
 import { coreSchema } from "./shared";
 
-export const userSchema = coreSchema.extend({
-	email: z.string().transform((val) => val.toLowerCase()),
-	emailVerified: z.boolean().default(false),
-	name: z.string(),
-	image: z.string().nullish(),
-});
+export const userSchema = coreSchema
+	.extend({
+		email: z.string(),
+		emailVerified: z.boolean().default(false),
+		name: z.string(),
+		image: z.string().nullish(),
+	})
+	.transform((data) => ({
+		...data,
+		email: data.email.toLowerCase(),
+	}));
 
 /**
  * User schema type used by better-auth, note that it's possible that user could have additional fields
